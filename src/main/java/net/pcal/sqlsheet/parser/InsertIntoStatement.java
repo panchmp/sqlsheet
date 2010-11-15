dcf6a5c103b1de114bbe901279e0b0712f9210c3
Michael <panchmp@gmail.com>
2010-11-15 19:49:17 +0000

    apply Ivan's changes

--- src/main/java/net/pcal/sqlsheet/parser/InsertIntoStatement.java
+++ src/main/java/net/pcal/sqlsheet/parser/InsertIntoStatement.java
@@ -19,17 +19,17 @@ import java.util.List;
 
 /**
  * Handle on a parsed SQL statement of the form
- * 
+ * <p/>
  * INSERT INTO xxx (xxx) VALUE (xxx).
- * 
+ *
  * @author <a href='http://www.pcal.net'>pcal</a>
  */
 public interface InsertIntoStatement extends ParsedStatement {
 
-  public String getTable();
+    public String getTable();
 
-  public List<String> getColumns();
+    public List<String> getColumns();
 
-  public List<Object> getValues();
+    public List<Object> getValues();
 
 }
